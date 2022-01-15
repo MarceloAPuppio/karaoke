71fdc51c5f7a809f42a85a8e90679332d2e9a8fa
Marcelo Puppio Desktop <marcelopuppio@gmail.com>
2022-01-15 02:28:43 -0300

    css and claslists.add

--- index.js
+++ index.js
@@ -1,12 +1,23 @@
-const audio = document.querySelector('video')
+const audio = document.querySelector('audio')
 const play = document.querySelector('.play')
 const stop = document.querySelector('.stop')
 play.addEventListener('click',()=>{audio.play(); console.log("play")})
-stop.addEventListener('click',()=>{audio.pause();console.log("stop")})
-audio.addEventListener('playing', (e)=>{console.log(e.target.currentTime)})
-if(audio.currentTime==1)console.log("1")
-if(audio.currentTime==2)console.log("2")
-if(audio.currentTime==3)console.log("3")
-if(audio.currentTime==4)console.log("4")
-if(audio.currentTime==5)console.log("5")
-if(audio.currentTime==6)console.log("6")
+stop.addEventListener('click',(e)=>{e.stopPropagation(); audio.pause();console.log("stop")})
+
+document.body.addEventListener('click',(e)=>{audio.play()})
+document.body.addEventListener('dblclick',(e)=>{audio.pause()})
+
+audio.textTracks[0].addEventListener('cuechange', function() {
+    document.getElementById('display').innerHTML = this.activeCues[0].text;
+    if(this.activeCues[0].text==""){
+        document.querySelector('img').classList.add("move")
+        document.body.classList.add("color")
+    } 
+        
+    else{
+        document.body.classList.remove("color")
+        document.querySelector('img').classList.remove("move")
+        
+    } 
+
+});
