ce9ba1a9d0609b26bc794d3dc6503c91a1a24421
Marcelo Puppio Desktop <marcelopuppio@gmail.com>
2022-01-16 00:21:04 -0300

    sprites

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
 const audio = document.querySelector('audio')
 const play = document.querySelector('.play')
+const fast = document.querySelector('.fast')
 const stop = document.querySelector('.stop')
 play.addEventListener('click',()=>{audio.play(); console.log("play")})
 stop.addEventListener('click',(e)=>{e.stopPropagation(); audio.pause();console.log("stop")})
+fast.addEventListener('click', (e)=>{e.stopPropagation();audio.currentTime+=5})
+fast.addEventListener('dblclick', (e)=>{e.stopPropagation();audio.currentTime+=10})
 
 document.body.addEventListener('click',(e)=>{audio.play()})
 document.body.addEventListener('dblclick',(e)=>{audio.pause()})
